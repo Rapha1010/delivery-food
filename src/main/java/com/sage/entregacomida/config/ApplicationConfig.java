245ec0b33bd667b6989c4e378e62929556ec5f03
Raphael Alves Tavares Leao <raphael.leao@ibm.com>
2022-08-05 14:01:13 -0300

    ajustando info

--- src/main/java/com/sage/entregacomida/config/ApplicationConfig.java
+++ src/main/java/com/sage/entregacomida/config/ApplicationConfig.java
@@ -21,11 +21,11 @@ public class ApplicationConfig implements CommandLineRunner  {
 		Cliente cliente1 = new Cliente(1L,"Joao da Silva", "joao.silva@ibm.com",994618524L);
 		Cliente cliente2 = new Cliente(2L,"Ana Maria", "ana.maria@ibm.com",994618522L);
 		Cliente cliente3 = new Cliente(3L,"Thiago Alves", "thiago.alves@ibm.com",994618523L);
-		Cliente cliente4 = new Cliente(4L,"Julia Souza", "juliana.souza@ibm.com",994618523L);
-		Cliente cliente5 = new Cliente(5L,"Igor Motta", "joao.silva@ibm.com",994618524L);
-		Cliente cliente6 = new Cliente(6L,"Teo Nunes", "ana.maria@ibm.com",994618522L);
-		Cliente cliente7 = new Cliente(7L,"Leonardo Martins", "thiago.alves@ibm.com",994618523L);
-		Cliente cliente8 = new Cliente(8L,"Paula Bordalo", "juliana.souza@ibm.com",994618523L);
+		Cliente cliente4 = new Cliente(4L,"Julia Souza", "julia.souza@ibm.com",994618523L);
+		Cliente cliente5 = new Cliente(5L,"Igor Motta", "igor.motta@ibm.com",994618524L);
+		Cliente cliente6 = new Cliente(6L,"Teo Nunes", "teo.nunes@ibm.com",994618522L);
+		Cliente cliente7 = new Cliente(7L,"Leonardo Martins", "leonardo.martins@ibm.com",994618523L);
+		Cliente cliente8 = new Cliente(8L,"Paula Bordalo", "paula.bordalo@ibm.com",994618523L);
 
 		clienteRepository.saveAll(Arrays.asList(cliente1, cliente2, cliente3, cliente4, cliente5, cliente6, cliente7, cliente8));
 		
